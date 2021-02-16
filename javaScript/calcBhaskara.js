8756bc3d728142b91357a6b395bdf8ab491fab5f
Fernando Churros <kappapridelgbt@gmail.com>
2021-02-16 10:51:15 -0300

    create var to receive A,B,C

--- javaScript/calcBhaskara.js
+++ javaScript/calcBhaskara.js
@@ -2,6 +2,10 @@ function calcBhaskara() {
    var A = window.document.getElementById("numA").value;
    var B = window.document.getElementById("numB").value;
    var C = window.document.getElementById("numC").value;
+   var reBhas = window.document.getElementById("receiveBhas");
 
-   window.document.getElementById("receiveBhas").innerHTML = `valor ${A}, valor ${B}, valor ${C}`
+   // window.document.getElementById("receiveBhas").innerHTML = `valor ${A}, valor ${B}, valor ${C}`
+
+   reBhas.innerHTML = `- ${B} &#177; &#8730; ${B}<sup>2</sup> - 4 . ${A} . ${C} / 2 . ${A}`
+   
 }
