d05f399efcc3a5d027c8d541cdb2e70bc77581ff
Fernando Churros <kappapridelgbt@gmail.com>
2021-02-16 13:35:59 -0300

    ends bhaskara calculator

--- javaScript/calcBhaskara.js
+++ javaScript/calcBhaskara.js
@@ -4,27 +4,24 @@ function calcBhaskara() {
    var C = window.document.getElementById("numC").value
    var reBhas = window.document.getElementById("receiveBhas")
    
+   var delta = ((B)**2) - 4 * A * (C)
+   var raizDelta = Math.sqrt(delta)
    
+   reBhas.innerHTML = "<p class='resultado'>-B &#177; &#8730; &#120491; / 2 . A </p>"
    reBhas.innerHTML += `<p>a= ${A}, b= ${B}, c= ${C}</p>`
    
-   if (B < 0 && C < 0) {
-      var delta = ((B)**2) - 4 * A * (C)
-      var raizDelta = Math.sqrt(delta)
-      // CALCULA O DELTA
-      reBhas.innerHTML += `<p>&#120491; = B<sup>2</sup> - 4.A.C</p>`
-      reBhas.innerHTML += `<p>&#120491; = (${B})<sup>2</sup> - 4.${A}.(${C}) </p>`
-      reBhas.innerHTML += `<p>&#120491; = ${(B)**2} + ${-4 * A * (C)} </p>`
-      reBhas.innerHTML += `<p>&#120491; = ${delta} </p>`
-
-      reBhas.innerHTML += `<br><p>x = -(${B}) &#177; &#8730;${delta} / 2 . ${A} </p>`
-      reBhas.innerHTML += `<p>x = ${-(B)} &#177; ${raizDelta} / ${2*A} </p>`
-
-      reBhas.innerHTML += `<br><p>x\' = ${-(B)} + ${raizDelta} / ${2*A} </p>`
-      reBhas.innerHTML += `<p>x\' = ${-(B) + raizDelta} / ${2 * A} = ${(-(B) + raizDelta) / 2 * A} </p>`
-
-      reBhas.innerHTML += `<br><p>x\' = ${-(B)} - ${raizDelta} / ${2*A} </p>`
-      reBhas.innerHTML += `<p>x\' = ${-(B) - raizDelta} / ${2 * A} = ${(-(B) - raizDelta) / 2 * A} </p>`
-   } else {
-      reBhas.innerHTML = "tá funcionando"
-   }
+   reBhas.innerHTML += `<p>&#120491; = B<sup>2</sup> - 4.A.C</p>`
+   reBhas.innerHTML += `<p>&#120491; = (${B})<sup>2</sup> - 4.${A}.(${C}) </p>`
+   reBhas.innerHTML += `<p>&#120491; = ${(B)**2} + ${-4 * A * (C)} </p>`
+   reBhas.innerHTML += `<p>&#120491; = ${delta} </p>`
+   
+   reBhas.innerHTML += `<br><p>x = -(${B}) &#177; &#8730;${delta} / 2 . ${A} </p>`
+   reBhas.innerHTML += `<p>x = ${-(B)} &#177; ${raizDelta} / ${2*A} </p>`
+   
+   reBhas.innerHTML += `<br><p>x\' = ${-(B)} + ${raizDelta} / ${2*A} </p>`
+   reBhas.innerHTML += `<p class="resultado">x\' = ${-(B) + raizDelta} / ${2 * A} = ${(-(B) + raizDelta) / 2 * A} </p>`
+   
+   reBhas.innerHTML += `<br><p>x\'\' = ${-(B)} - ${raizDelta} / ${2*A} </p>`
+   reBhas.innerHTML += `<p class="resultado">x\'\' = ${-(B) - raizDelta} / ${2 * A} = ${(-(B) - raizDelta) / 2 * A} </p>`
+   
 }
