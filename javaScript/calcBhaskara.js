1632fd42d0a119fb3420dfad9f854287762a904a
Fernando Churros <kappapridelgbt@gmail.com>
2021-02-16 10:38:56 -0300

    create js test

---
+++ javaScript/calcBhaskara.js
@@ -0,0 +1,7 @@
+function calcBhaskara() {
+   var A = window.document.getElementById("numA").value;
+   var B = window.document.getElementById("numB").value;
+   var C = window.document.getElementById("numC").value;
+
+   window.document.getElementById("receiveBhas").innerHTML = `valor ${A}, valor ${B}, valor ${C}`
+}
