e782a4f67c8281738ab139ceeded7a18c46d8e49
FernandoChurros <kappapridelgbt@gmail.com>
2021-03-01 21:30:34 -0300

    refactor show bhaskara

--- javaScript/calcBhaskara.js
+++ javaScript/calcBhaskara.js
@@ -1,27 +1,37 @@
 function calcBhaskara() {
-   var A = window.document.getElementById("numA").value
-   var B = window.document.getElementById("numB").value
-   var C = window.document.getElementById("numC").value
-   var reBhas = window.document.getElementById("receiveBhas")
-   
-   var delta = ((B)**2) - 4 * A * (C)
-   var raizDelta = Math.sqrt(delta)
-   
-   reBhas.innerHTML = "<p class='resultado'>-B &#177; &#8730; &#120491; / 2 . A </p>"
-   reBhas.innerHTML += `<p>a= ${A}, b= ${B}, c= ${C}</p>`
-   
-   reBhas.innerHTML += `<p>&#120491; = B<sup>2</sup> - 4.A.C</p>`
-   reBhas.innerHTML += `<p>&#120491; = (${B})<sup>2</sup> - 4.${A}.(${C}) </p>`
-   reBhas.innerHTML += `<p>&#120491; = ${(B)**2} + ${-4 * A * (C)} </p>`
-   reBhas.innerHTML += `<p>&#120491; = ${delta} </p>`
-   
-   reBhas.innerHTML += `<br><p>x = -(${B}) &#177; &#8730;${delta} / 2 . ${A} </p>`
-   reBhas.innerHTML += `<p>x = ${-(B)} &#177; ${raizDelta} / ${2*A} </p>`
-   
-   reBhas.innerHTML += `<br><p>x\' = ${-(B)} + ${raizDelta} / ${2*A} </p>`
-   reBhas.innerHTML += `<p class="resultado">x\' = ${-(B) + raizDelta} / ${2 * A} = ${(-(B) + raizDelta) / 2 * A} </p>`
-   
-   reBhas.innerHTML += `<br><p>x\'\' = ${-(B)} - ${raizDelta} / ${2*A} </p>`
-   reBhas.innerHTML += `<p class="resultado">x\'\' = ${-(B) - raizDelta} / ${2 * A} = ${(-(B) - raizDelta) / 2 * A} </p>`
-   
+
+   let A = window.document.getElementById("numA").value
+   let B = window.document.getElementById("numB").value
+   let C = window.document.getElementById("numC").value
+   const reBhas = window.document.getElementById("receiveBhas")
+
+   let delta = ((B) ** 2) - 4 * A * (C)
+   let raizDelta = Math.sqrt(delta)
+
+   function createElemento(tag, content, atributo) {
+      let elemento = document.createElement(tag);
+      elemento.classList.add(atributo);
+      elemento.textContent = content;
+      reBhas.appendChild(elemento);
+   }
+
+   reBhas.innerHTML = "<p class='resultado'> -B ± √ 𝚫 / 2 . A </p>"
+   createElemento('p', `a= ${A}, b= ${B}, c= ${C}`);
+
+   createElemento('p', `𝚫 = B \u00B2 - 4.A.C`);
+   createElemento('p', `𝚫 = (${B})\u00B2 - 4.${A}.(${C})`);
+   createElemento('p', `𝚫 = ${(B)**2} + ${-4 * A * (C)}`);
+   createElemento('p', `𝚫 = ${delta}`);
+
+
+   createElemento('p', `x = -(${B}) ± √${delta} / 2 . ${A}`);
+   createElemento('p', `x = ${-(B)} ± ${raizDelta} / ${2*A} `);
+
+   createElemento('p', `x\' = ${-(B)} + ${raizDelta} / ${2*A}`);
+   createElemento('p', `x\' = ${-(B) + raizDelta} / ${2 * A} = ${(-(B) + raizDelta) / 2 * A}`, 'resultado');
+
+   createElemento('p', `x\'\' = ${-(B)} - ${raizDelta} / ${2*A}`);
+   createElemento('p', `x\'\' = ${-(B) - raizDelta} / ${2 * A} = ${(-(B) - raizDelta) / 2 * A}`, 'resultado');
 }
+
+// &#120491; = 𝚫  &#177; = ±   &#8730; = √
